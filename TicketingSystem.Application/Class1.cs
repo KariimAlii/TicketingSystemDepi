868a91865dd38db56c1800089a26a540ffaef7b2
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-05-10 21:17:51 +0300

    Added Get Ticketing Feature

--- TicketingSystem.Application/Class1.cs
+++ TicketingSystem.Application/Class1.cs
@@ -7,5 +7,7 @@
         public string FirstName {get; set;}
         public string LastName {get; set;}
         public string Department {get; set;}
+        public string Salary {get; set;}
+        public string Address {get; set;}
     }
 }
